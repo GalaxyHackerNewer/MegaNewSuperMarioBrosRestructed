35f0fab1e0322e932dfdd2d52b0a91b69ecb2396
galaxyhacker newer <72502880+GalaxyHackerNewer@users.noreply.github.com>
2023-05-24 03:26:42 +0200

    Add files via upload
    Mores Sprites Added 0eme Pack

---
+++ sprites/Crossemessagename/textures_tpl.h
@@ -0,0 +1,6 @@
+# pragma once #
+#include stdio.h
+#include stdlib.h
+textures emplacements
+C:/Users/gregv/Documents/GitHub/MegaNewSuperMarioBrosRestructed/sprites/Crossemessagename/6175e31a-efaa-4edb-813d-86c8bae9ba45.png
+C:/Users/gregv/Documents/Github/MegaNewSuperMarioBrosRestructed/sprites/PlumberHead/11c287eb-9c58-4033-b32a-d1074c0c22c1.png
