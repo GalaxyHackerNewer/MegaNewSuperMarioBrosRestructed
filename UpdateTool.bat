0356ac13ea8c46b2744f8037c7bf1b0d566b2000
galaxyhacker newer <72502880+GalaxyHackerNewer@users.noreply.github.com>
2023-05-24 03:19:16 +0200

    Adding UpdateTool Batch

---
+++ UpdateTool.bat
@@ -0,0 +1,26 @@
+@echo off
+@echo off
+printf("Hello World!");
+printf("Welcome To Mega New Super Mario Bros Update Tool\n");
+printf("Made By GhnYoutubeChannel Soon GhngamingLive\n");
+printf("Searching Update Please Wait.........\n");
+@echo off
+printf("Pjkoli Patrol Error\n");
+printf("PJKOLI PATROL ERROR!!!\n");
+printf("No Update Found\n");
+@echo off
+printf("Thanks for use Mega New Super Mario Bros Update Tool\n");
+printf("Done\n");
+@echo off
+printf("visit my official website https://gregvall1999.wixsite.com/ghnse for found gaming and hacking\n");
+@echo off
+printf("or follow me on facebook profile https://www.facebook.com/profile.php?id=100010601143198\n");
+printf("facebook page https://www.facebook.com/GlsGamingCommunity\n");
+printf("youtube channel https://www.youtube.com/channel/UCNgYkG2CZAw48XV3xO4-TsQ\n");
+printf("twitter https://twitter.com/gregorylesnaps2\n");
+printf("discord https://discord.com/invite/MjYjbVeptJ\n");
+printf("instagram https://instagram.com/glsgamingcommunity\n");
+printf("steam https://steamcommunity.com/id/Galaxyhackernewer/myworkshopfiles/\n");
+printf("gamebanana https://gamebanana.com/members/2087873\n");
+printf("github https://github.com/GalaxyHackerNewer\n");
+
