af0ce5aca9fa0a6025e373dc653a0e7002cee7c3
galaxyhacker newer <72502880+GalaxyHackerNewer@users.noreply.github.com>
2023-06-25 04:15:35 +0200

    Batch UpdateTool Update
    Mise a jour du fichier de commande pour mettre a jour le jeu

--- UpdateTool.bat
+++ UpdateTool.bat
@@ -1,26 +1,41 @@
-@echo off
-@echo off
-printf("Hello World!");
-printf("Welcome To Mega New Super Mario Bros Update Tool\n");
-printf("Made By GhnYoutubeChannel Soon GhngamingLive\n");
-printf("Searching Update Please Wait.........\n");
-@echo off
-printf("Pjkoli Patrol Error\n");
-printf("PJKOLI PATROL ERROR!!!\n");
-printf("No Update Found\n");
-@echo off
-printf("Thanks for use Mega New Super Mario Bros Update Tool\n");
-printf("Done\n");
-@echo off
-printf("visit my official website https://gregvall1999.wixsite.com/ghnse for found gaming and hacking\n");
-@echo off
-printf("or follow me on facebook profile https://www.facebook.com/profile.php?id=100010601143198\n");
-printf("facebook page https://www.facebook.com/GlsGamingCommunity\n");
-printf("youtube channel https://www.youtube.com/channel/UCNgYkG2CZAw48XV3xO4-TsQ\n");
-printf("twitter https://twitter.com/gregorylesnaps2\n");
-printf("discord https://discord.com/invite/MjYjbVeptJ\n");
-printf("instagram https://instagram.com/glsgamingcommunity\n");
-printf("steam https://steamcommunity.com/id/Galaxyhackernewer/myworkshopfiles/\n");
-printf("gamebanana https://gamebanana.com/members/2087873\n");
-printf("github https://github.com/GalaxyHackerNewer\n");
+
+
+@echo on
+@echo on
+rem("Hello World!");
+rem("Welcome To Mega New Super Mario Bros Update Tool\n");
+rem"Made By GhnYoutubeChannel Soon GhngamingLive\n");
+rem("Searching Update Please Wait.........\n");
+@echo on
+rem("Pjkoli Patrol Error\n");
+rem("PJKOLI PATROL ERROR!!!\n");
+rem("No Update Found\n");
+@echo on
+rem("Thanks for use Mega New Super Mario Bros Update Tool\n");
+rem("Done\n");
+@echo on
+rem("visit my official website https://gregvall1999.wixsite.com/ghnse for found gaming and hacking\n");
+@echo on
+rem("or follow me on facebook profile https://www.facebook.com/profile.php?id=100010601143198\n");
+rem("facebook page https://www.facebook.com/GlsGamingCommunity\n");
+rem("youtube channel https://www.youtube.com/channel/UCNgYkG2CZAw48XV3xO4-TsQ\n");
+rem("twitter https://twitter.com/gregorylesnaps2\n");
+rem("discord https://discord.com/invite/MjYjbVeptJ\n");
+rem("instagram https://instagram.com/glsgamingcommunity\n");
+rem("steam https://steamcommunity.com/id/Galaxyhackernewer/myworkshopfiles/\n");
+rem("gamebanana https://gamebanana.com/members/2087873\n");
+rem("github https://github.com/GalaxyHackerNewer\n");
+rem("please enter your name\n");
+@echo on
+ @echo on
+ 
+SET nom=
+SET /p nom=Votre nom:
+echo hello %nom%
+
+
+
+@echo on
+set dossier=%programfiles%\VideoLAN\VLC\
+"%dossier%\vlc.exe" -vvv --codec ffmpeg C:\Users\gregv\Documents\Github\MegaNewSuperMarioBrosRestructed\SSBB-Main_Theme-AJ.sound
 
