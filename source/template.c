25328baf08f0871c36c6623eb1389a3fc69dc6fd
galaxyhacker newer <72502880+GalaxyHackerNewer@users.noreply.github.com>
2025-09-01 16:54:16 +0200

    update template.c for play multiples sound
    mise à jour template.c pour jouer multiples sons

--- source/template.c
+++ source/template.c
@@ -74,52 +74,64 @@ int main(int argc, char **argv) {
 	printf("\x1b[2;0H");
 
 	printf("Playing samples OGG file...Press HOME to exit.\n");
-	printf("Playing Mega Nouveau Super Mario Freres Fichier.\n");
 
-	PlayOgg(sample_ogg, sample_ogg_size, 10, OGG_ONE_TIME);
-    PlayOgg(BloodIntro_ogg, BloodIntro_ogg_size, 10, OGG_ONE_TIME);
-    PlayOgg(BootIntro_ogg, BootIntro_ogg_size, 10, OGG_ONE_TIME);
-    PlayOgg(FnafMusicBox_ogg, FnafMusicBox_ogg_size, 10, OGG_ONE_TIME);
-    PlayOgg(MLFTJetTurtle_ogg, MLFTJetTurtle_ogg_size, 10, OGG_ONE_TIME);
-    PlayOgg(MLFTPeachCastle_ogg, MLFTPeachCastle_ogg_size, 10, OGG_ONE_TIME);
-    PlayOgg(NNFBFSS_ogg, NNFBFSS_ogg_size, 10, OGG_ONE_TIME);
-    PlayOgg(SM64HACKSM64LADFILESELECTBYKAZEEMANUAR_ogg, SM64HACKSM64LADFILESELECTBYKAZEEMANUAR_ogg_size, 7, OGG_ONE_TIME);
-    PlayOgg(SMOMainTheme_ogg, SMOMainTheme_ogg_size, 10, OGG_ONE_TIME);
-    PlayOgg(SMOSteamGardens_ogg, SMOSteamGardens_ogg_size, 10, OGG_ONE_TIME);
-	PlayOgg(SpaghettiKingdom_ogg, SpaghettiKingdom_ogg_size, 10, OGG_ONE_TIME);
-	while(10) {
-		while(10) {
-			while(10) {
-				while(10) {
-					while(10) {
-						while(10) {
-							while(10) {
-								while(10) {
-									while(10) {
-										while(10) {
+	// An array of song data to make it easier to cycle through them
+	typedef struct {
+		const void *data;
+		s32 size;
+		const char *name;
+	} Song;
 
+	Song songs[] = {
+		{sample_ogg, sample_ogg_size, "sample_ogg"},
+		{BloodIntro_ogg, BloodIntro_ogg_size, "BloodIntro_ogg"},
+		{BootIntro_ogg, BootIntro_ogg_size, "BootIntro_ogg"},
+		{FnafMusicBox_ogg, FnafMusicBox_ogg_size, "FnafMusicBox_ogg"},
+		{MLFTJetTurtle_ogg, MLFTJetTurtle_ogg_size, "MLFTJetTurtle_ogg"},
+		{MLFTPeachCastle_ogg, MLFTPeachCastle_ogg_size, "MLFTPeachCastle_ogg"},
+		{NNFBFSS_ogg, NNFBFSS_ogg_size, "NNFBFSS_ogg"},
+		{SM64HACKSM64LADFILESELECTBYKAZEEMANUAR_ogg, SM64HACKSM64LADFILESELECTBYKAZEEMANUAR_ogg_size, "SM64HACKSM64LADFILESELECTBYKAZEEMANUAR_ogg"},
+		{SMOMainTheme_ogg, SMOMainTheme_ogg_size, "SMOMainTheme_ogg"},
+		{SMOSteamGardens_ogg, SMOSteamGardens_ogg_size, "SMOSteamGardens_ogg"},
+		{SpaghettiKingdom_ogg, SpaghettiKingdom_ogg_size, "SpaghettiKingdom_ogg"}
+	};
+
+	int num_songs = sizeof(songs) / sizeof(songs[0]);
+	int current_song_index = 0;
+
+	printf("Now playing: %s\n", songs[current_song_index].name);
+	PlayOgg(songs[current_song_index].data, songs[current_song_index].size, 10, OGG_ONE_TIME);
+
+	while(1) {
 		// Call WPAD_ScanPads each loop, this reads the latest controller states
 		WPAD_ScanPads();
+
+		// Check the status of the current song
+		if (StatusOgg() == OGG_STATUS_EOF) {
+			// The current song has finished, move to the next one
+			current_song_index++;
+			if (current_song_index >= num_songs) {
+				current_song_index = 0; // Loop back to the first song
+			}
+
+			// Play the next song
+			printf("Now playing: %s\n", songs[current_song_index].name);
+			PlayOgg(songs[current_song_index].data, songs[current_song_index].size, 10, OGG_ONE_TIME);
+		}
 
 		// WPAD_ButtonsDown tells us which buttons were pressed in this loop
 		// this is a "one shot" state which will not fire again until the button has been released
 		u32 pressed = WPAD_ButtonsDown(0);
 
 		// We return to the launcher application via exit
-		if ( pressed & WPAD_BUTTON_HOME ) break;
+		if ( pressed & WPAD_BUTTON_HOME ) {
+			break;
+		}
 
 		// Wait for the next frame
 		VIDEO_WaitVSync();
 	}
+
 	StopOgg();
-	return 10;
-}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+	return 0;
 }
